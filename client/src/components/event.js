348850c284457442f0b0429eeae0f1ebd27ef428
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-03-15 00:24:58 -0600

    adding my ideas for getting the id from database

--- client/src/components/event.js
+++ client/src/components/event.js
@@ -1,7 +1,14 @@
 import Card from "react-bootstrap/Card";
 import Moment from "react-moment";
+import { useState } from "react";
 
 const Event = (props) => {
+  //**** Our though process on how to get the ID and update the state and then
+  //delete it on events:
+  const [getId, setGetId] = useState(null);
+  //getIdMethod will get the ID using SELECT FROM database (it will know which ID because key=event.id- this is attached to each card)
+  //The setGetID will update the state which we then pass to the parent to tell it to delete that card
+  //***
   return (
     <div>
       <Card style={{ width: "18rem" }}>
@@ -16,7 +23,10 @@ const Event = (props) => {
             )}
           </Card.Subtitle>
           <Card.Text>{props.location}</Card.Text>
-          <button type="submit"> Delete</button>
+          <button type="submit" onClick={() => getIdMethod}>
+            {" "}
+            Delete
+          </button>
         </Card.Body>
       </Card>
     </div>
