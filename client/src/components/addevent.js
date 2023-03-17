099484cb0614aecb6e3ced753cd62528cd3288be
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-03-16 22:38:16 -0600

    css again lol

--- client/src/components/addevent.js
+++ client/src/components/addevent.js
@@ -37,34 +37,36 @@ const AddEvent = (props) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Title</label>
-      <input
-        type="text"
-        id="add-event-title"
-        placeholder="The Title of your Event"
-        required
-        value={event.title} //state
-        onChange={handleTitleChange}
-      />
-      <label>Place</label>
-      <input
-        type="text"
-        id="add-event-location"
-        placeholder="The Location of your Event"
-        required
-        value={event.location} //state
-        onChange={handleLocationChange}
-      />
-      <label>Date</label>
-      <input
-        type="date"
-        id="add-event-date"
-        value={event.eventtime} //state
-        onChange={handleDateChange}
-      />
-      <button type="submit">Add Event</button>
-    </form>
+    <span id="form">
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input
+          type="text"
+          id="add-event-title"
+          placeholder="The Title of your Event"
+          required
+          value={event.title} //state
+          onChange={handleTitleChange}
+        />
+        <label>Place</label>
+        <input
+          type="text"
+          id="add-event-location"
+          placeholder="The Location of your Event"
+          required
+          value={event.location} //state
+          onChange={handleLocationChange}
+        />
+        <label>Date</label>
+        <input
+          type="date"
+          id="add-event-date"
+          value={event.eventtime} //state
+          onChange={handleDateChange}
+        />
+        <button type="submit">Add Event</button>
+      </form>
+    </span>
   );
 };
 
