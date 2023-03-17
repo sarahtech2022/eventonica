598fb5cf3172305e2f3e712fedfbf6f64be09ed5
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-03-17 16:24:48 -0600

    more css

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,10 @@ function App() {
       <span id="titlespan">
         <h1 id="title">Techonica 2023 events</h1>
       </span>
+      <img
+        id="eventimg"
+        src="https://thenounproject.com/api/private/icons/1430241/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0&token=gAAAAABkFOdC4gDh-7H7FpdH7CupvWItWMBgUmTUqMU6ouHYsM_MLKjIK_cN64bFnqu4uufzueOD0roe95yYmst1y8vMhtI2AQ%3D%3D"
+      ></img>
       <Events />
     </div>
   );
